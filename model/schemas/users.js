7bdaa4f2a3d9d70bdc3f900d48089a9b72c00179
freethinkel <ascuns05@icloud.com>
2018-06-09 00:35:53 +0300

    fix

--- model/schemas/users.js
+++ model/schemas/users.js
@@ -1,17 +1,9 @@
 module.exports = function (mongo) {
 
-  const answerSchema = mongo.Schema({
-    qId: String
-    , answer: String
-    , isCorrect: Boolean
-    , points: Number
-    , type: Boolean
-  }, { _id: false });
-
-
   return mongo.Schema({
     firstName: String
     , lastName: String
     , group: String
-  })
-}
+    , countComplitedQuests: Number
+  });
+};
